ecbf942f6afc96fe3e912d5971b03be33bce61c0
yzehao <yzehao@thoughtworks.com>
2020-05-19 00:15:51 +0800

    refactor hard code string to static final variable

--- src/main/java/cn/xpbootcamp/gilded_rose/GildedRose.java
+++ src/main/java/cn/xpbootcamp/gilded_rose/GildedRose.java
@@ -1,6 +1,10 @@
 package cn.xpbootcamp.gilded_rose;
 
 class GildedRose {
+    private static final String AGED_BRIE = "Aged Brie";
+    private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+    private static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+    
     Product[] products;
 
     public GildedRose(Product[] products) {
@@ -9,10 +13,10 @@ class GildedRose {
 
     public void updateAllProductsPassOneDay() {
         for (int i = 0; i < products.length; i++) {
-            if (!products[i].getName().equals("Aged Brie")
-                    && !products[i].getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (!products[i].getName().equals(AGED_BRIE)
+                    && !products[i].getName().equals(BACKSTAGE_PASSES)) {
                 if (products[i].getQuality() > 0) {
-                    if (!products[i].getName().equals("Sulfuras, Hand of Ragnaros")) {
+                    if (!products[i].getName().equals(SULFURAS)) {
                         products[i].setQuality(products[i].getQuality() - 1);
                     }
                 }
@@ -20,7 +24,7 @@ class GildedRose {
                 if (products[i].getQuality() < 50) {
                     products[i].setQuality(products[i].getQuality() + 1);
 
-                    if (products[i].getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (products[i].getName().equals(BACKSTAGE_PASSES)) {
                         if (products[i].getSellIn() < 11) {
                             if (products[i].getQuality() < 50) {
                                 products[i].setQuality(products[i].getQuality() + 1);
@@ -36,15 +40,15 @@ class GildedRose {
                 }
             }
 
-            if (!products[i].getName().equals("Sulfuras, Hand of Ragnaros")) {
+            if (!products[i].getName().equals(SULFURAS)) {
                 products[i].setSellIn(products[i].getSellIn() - 1);
             }
 
             if (products[i].getSellIn() < 0) {
-                if (!products[i].getName().equals("Aged Brie")) {
-                    if (!products[i].getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (!products[i].getName().equals(AGED_BRIE)) {
+                    if (!products[i].getName().equals(BACKSTAGE_PASSES)) {
                         if (products[i].getQuality() > 0) {
-                            if (!products[i].getName().equals("Sulfuras, Hand of Ragnaros")) {
+                            if (!products[i].getName().equals(SULFURAS)) {
                                 products[i].setQuality(products[i].getQuality() - 1);
                             }
                         }
