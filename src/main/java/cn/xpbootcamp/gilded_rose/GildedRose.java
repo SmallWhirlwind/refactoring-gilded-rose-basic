85e92a2d5cce4bd7856580d9fcf1f2420e54145b
yzehao <yzehao@thoughtworks.com>
2020-05-19 18:17:06 +0800

    extract reset legitimate quality to method

--- src/main/java/cn/xpbootcamp/gilded_rose/GildedRose.java
+++ src/main/java/cn/xpbootcamp/gilded_rose/GildedRose.java
@@ -4,7 +4,7 @@ class GildedRose {
     private static final String AGED_BRIE = "Aged Brie";
     private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
     private static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
-    
+
     Product[] products;
 
     public GildedRose(Product[] products) {
@@ -15,27 +15,19 @@ class GildedRose {
         for (int i = 0; i < products.length; i++) {
             if (!products[i].getName().equals(AGED_BRIE)
                     && !products[i].getName().equals(BACKSTAGE_PASSES)) {
-                if (products[i].getQuality() > 0) {
-                    if (!products[i].getName().equals(SULFURAS)) {
-                        products[i].setQuality(products[i].getQuality() - 1);
-                    }
+                if (!products[i].getName().equals(SULFURAS)) {
+                    products[i].setQuality(products[i].getQuality() - 1);
                 }
             } else {
-                if (products[i].getQuality() < 50) {
-                    products[i].setQuality(products[i].getQuality() + 1);
+                products[i].setQuality(products[i].getQuality() + 1);
 
-                    if (products[i].getName().equals(BACKSTAGE_PASSES)) {
-                        if (products[i].getSellIn() < 11) {
-                            if (products[i].getQuality() < 50) {
-                                products[i].setQuality(products[i].getQuality() + 1);
-                            }
-                        }
+                if (products[i].getName().equals(BACKSTAGE_PASSES)) {
+                    if (products[i].getSellIn() < 11) {
+                        products[i].setQuality(products[i].getQuality() + 1);
+                    }
 
-                        if (products[i].getSellIn() < 6) {
-                            if (products[i].getQuality() < 50) {
-                                products[i].setQuality(products[i].getQuality() + 1);
-                            }
-                        }
+                    if (products[i].getSellIn() < 6) {
+                        products[i].setQuality(products[i].getQuality() + 1);
                     }
                 }
             }
@@ -47,20 +39,27 @@ class GildedRose {
             if (products[i].getSellIn() < 0) {
                 if (!products[i].getName().equals(AGED_BRIE)) {
                     if (!products[i].getName().equals(BACKSTAGE_PASSES)) {
-                        if (products[i].getQuality() > 0) {
-                            if (!products[i].getName().equals(SULFURAS)) {
-                                products[i].setQuality(products[i].getQuality() - 1);
-                            }
+                        if (!products[i].getName().equals(SULFURAS)) {
+                            products[i].setQuality(products[i].getQuality() - 1);
                         }
                     } else {
                         products[i].setQuality(0);
                     }
                 } else {
-                    if (products[i].getQuality() < 50) {
-                        products[i].setQuality(products[i].getQuality() + 1);
-                    }
+                    products[i].setQuality(products[i].getQuality() + 1);
                 }
             }
+
+            resetLegitimateQuality(products[i]);
+        }
+    }
+
+    private void resetLegitimateQuality(Product product) {
+        if (product.getQuality() > 50) {
+            product.setQuality(50);
+        }
+        if (product.getQuality() < 0) {
+            product.setQuality(0);
         }
     }
 }
