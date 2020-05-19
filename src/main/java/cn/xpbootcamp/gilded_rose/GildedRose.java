6bbb9c511855a0e1788d23af3ca4c9b3a1ae7c64
yzehao <yzehao@thoughtworks.com>
2020-05-19 19:03:10 +0800

    refactor move product logic to product class

--- src/main/java/cn/xpbootcamp/gilded_rose/GildedRose.java
+++ src/main/java/cn/xpbootcamp/gilded_rose/GildedRose.java
@@ -1,9 +1,6 @@
 package cn.xpbootcamp.gilded_rose;
 
 class GildedRose {
-    private static final String AGED_BRIE = "Aged Brie";
-    private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
-    private static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
 
     Product[] products;
 
@@ -13,66 +10,17 @@ class GildedRose {
 
     public void updateAllProductsPassOneDay() {
         for (Product product : products) {
-            if (this.isCommonProduct(product)) {
-                updateCommonProductPassOneDay(product);
+            if (product.isCommonProduct()) {
+                product.updateCommonProductPassOneDay();
             }
-            if (product.getName().equals(AGED_BRIE)) {
-                updateAgedBriePassOneDay(product);
+            if (product.isAgedBrieProduct()) {
+                product.updateAgedBriePassOneDay();
             }
-            if (product.getName().equals(BACKSTAGE_PASSES)) {
-                updateBackStagePassesPassOneDay(product);
+            if (product.isBackStagePassesProduct()) {
+                product.updateBackStagePassesPassOneDay();
             }
-
-            resetLegitimateQuality(product);
+            product.resetLegitimateQuality();
         }
     }
 
-    private void updateBackStagePassesPassOneDay(Product product) {
-        product.setQuality(product.getQuality() + 1);
-
-        if (product.getSellIn() < 11) {
-            product.setQuality(product.getQuality() + 1);
-        }
-
-        if (product.getSellIn() < 6) {
-            product.setQuality(product.getQuality() + 1);
-        }
-        product.setSellIn(product.getSellIn() - 1);
-
-        if (product.getSellIn() < 0) {
-            product.setQuality(0);
-        }
-    }
-
-    private void updateAgedBriePassOneDay(Product product) {
-        product.setQuality(product.getQuality() + 1);
-        product.setSellIn(product.getSellIn() - 1);
-
-        if (product.getSellIn() < 0) {
-            product.setQuality(product.getQuality() + 1);
-        }
-    }
-
-    private void updateCommonProductPassOneDay(Product product) {
-        product.setQuality(product.getQuality() - 1);
-        product.setSellIn(product.getSellIn() - 1);
-        if (product.getSellIn() < 0) {
-            product.setQuality(product.getQuality() - 1);
-        }
-    }
-
-    private boolean isCommonProduct(Product product) {
-        return !product.getName().equals(AGED_BRIE)
-                && !product.getName().equals(BACKSTAGE_PASSES)
-                && !product.getName().equals(SULFURAS);
-    }
-
-    private void resetLegitimateQuality(Product product) {
-        if (product.getQuality() > 50) {
-            product.setQuality(50);
-        }
-        if (product.getQuality() < 0) {
-            product.setQuality(0);
-        }
-    }
 }
--- src/main/java/cn/xpbootcamp/gilded_rose/Product.java
+++ src/main/java/cn/xpbootcamp/gilded_rose/Product.java
@@ -2,6 +2,10 @@ package cn.xpbootcamp.gilded_rose;
 
 public class Product {
 
+    private static final String AGED_BRIE = "Aged Brie";
+    private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+    private static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+
     private String name;
 
     private int sellIn;
@@ -37,5 +41,62 @@ public class Product {
     @Override
     public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
+    }
+
+    public void updateBackStagePassesPassOneDay() {
+        this.setQuality(this.getQuality() + 1);
+
+        if (this.getSellIn() < 11) {
+            this.setQuality(this.getQuality() + 1);
+        }
+
+        if (this.getSellIn() < 6) {
+            this.setQuality(this.getQuality() + 1);
+        }
+        this.setSellIn(this.getSellIn() - 1);
+
+        if (this.getSellIn() < 0) {
+            this.setQuality(0);
+        }
+    }
+
+    public void updateAgedBriePassOneDay() {
+        this.setQuality(this.getQuality() + 1);
+        this.setSellIn(this.getSellIn() - 1);
+
+        if (this.getSellIn() < 0) {
+            this.setQuality(this.getQuality() + 1);
+        }
+    }
+
+    public void updateCommonProductPassOneDay() {
+        this.setQuality(this.getQuality() - 1);
+        this.setSellIn(this.getSellIn() - 1);
+        if (this.getSellIn() < 0) {
+            this.setQuality(this.getQuality() - 1);
+        }
+    }
+
+    public boolean isCommonProduct() {
+        return !this.getName().equals(AGED_BRIE)
+                && !this.getName().equals(BACKSTAGE_PASSES)
+                && !this.getName().equals(SULFURAS);
+    }
+
+    public void resetLegitimateQuality() {
+        if (this.getQuality() > 50) {
+            this.setQuality(50);
+        }
+        if (this.getQuality() < 0) {
+            this.setQuality(0);
+        }
+    }
+
+    public boolean isAgedBrieProduct() {
+        return this.getName().equals(AGED_BRIE);
+    }
+
+    public boolean isBackStagePassesProduct() {
+        return this.getName().equals(BACKSTAGE_PASSES);
     }
 }
